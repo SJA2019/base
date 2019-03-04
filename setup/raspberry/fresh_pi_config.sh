9f9736e25609247759628bc0c501ed1491f1ea73
Arjun <Arjun.Jayachandran@echostar.com>
2019-03-04 19:32:50 +0530

    more fixes/

--- setup/raspberry/fresh_pi_config.sh
+++ setup/raspberry/fresh_pi_config.sh
@@ -4,17 +4,19 @@
 #
 sudo echo ""
 sudo echo "#performing custom config on boot.."
-sudo echo "cd `pwd`" >> ~/.bashrc
-sudo echo "sh on_boot.sh >> ~/ob_boot_log.txt" >> ~/.bashrc
-sudo echo "cd -" >> ~/.bashrc
+#sudo echo "cd `pwd`" >> ~/.bashrc
+#sudo echo "rm ~/Desktop/ob_boot_log.txt >> ~/Desktop/ob_boot_log.txt" >> ~/.bashrc
+#sudo echo "sh on_boot.sh >> ~/Desktop/ob_boot_log.txt" >> ~/.bashrc
+#sudo echo "cd -" >> ~/.bashrc
+chmod ugo+x  
 
 
 
 # manual installs.
 #
-echo "set correct display resolution: 720x680 16:9 60hz seems to work."
+echo "set correct display resolution: 720x480 16:9 60hz seems to work."
 echo "enable sftp,ssh,vnc & static-ip"
 echo "enable video-camera-codecs-option, 256mb-memory-split-option, expanded-file-system in raspi-config,"
 echo "launching raspiconfig in 10s..."
 sleep 10
-raspi-config 
+sudo raspi-config 
