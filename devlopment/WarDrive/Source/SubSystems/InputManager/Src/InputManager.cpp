f35baf140616634caf80034c4cae5a4e35db9d2a
Arjun <Arjun.Jayachandran@echostar.com>
2020-01-02 10:12:09 +0530

    Joystick Init.

--- devlopment/WarDrive/Source/SubSystems/InputManager/Src/InputManager.cpp
+++ devlopment/WarDrive/Source/SubSystems/InputManager/Src/InputManager.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include "SDL.h"
 #include "InputManager.h"
 
+
 InputManager::InputManager() {
-	std::cout<<"InputManager() ++ --"<<std::endl;
+	std::cout<<"InputManager() ++"<<std::endl;
+
+
+	if( SDL_Init ( SDL_INIT_JOYSTICK ) < 0 )
+	{
+		std::cout<<"InputManager(): SDL_INIT_JOYSTICK failed!!"<<std::endl;	
+	}
+
+	std::cout<<"InputManager(): Number of joysticks found: "<<SDL_NumJoysticks()<<std::endl;	
+
+	std::cout<<"InputManager() ++"<<std::endl;
 }
 
 InputManager::~InputManager() {
-	std::cout<<"~InputManager() ++ --"<<std::endl;
+	std::cout<<"~InputManager() ++"<<std::endl;
+	SDL_Quit();
+	std::cout<<"~InputManager() --"<<std::endl;
 }
